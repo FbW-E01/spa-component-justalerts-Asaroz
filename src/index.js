d5e622975d8633e9be136c8ea611356c0c212784
Asaroz <77623721+Asaroz@users.noreply.github.com>
2021-07-08 13:51:50 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Box = (/* ???? */) => {
+const Box = (prop) => {
+  if(prop.hide){
+    return <div className={`alert alert-primary`} style={{display:"none"}} role="alert">
+                    {prop.message}
+           </div>
+  }
+    return <div>
+                <div className={`alert alert-primary`} style={{backgroundColor:prop.type}} role="alert">
+                     {prop.message}
+                </div>
 
-  // ?????
-  
+    </div>
 };
 
 // Do not edit below this line
